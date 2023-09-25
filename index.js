7762a8eb00a3a11d3d36ee3bd63c77ef597f4186
Elena <inculet98@mail.ru>
2023-09-25 20:27:26 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,123 @@
+// const camelCaseNamingStyle = "camelCase";
+// const UPPER_SNAKE_CASE_VARIABILE_NAMING_STALY = "UPPER_SNAKE_CASE_VARIABILE_NAMING_STALY";
+// const Car = "car";
+
+// console.log("Hey do you use ${cameCaselNamingStyle}, ${UPPER_SNAKE_CASE_VARIABILE_NAMING_STALY} or ${Capital}, ${car}")
+
+// const num = '3'
+// console.log(typeof +num)
+// console.log(typeof Number(num))
+
+
+// const str = 3
+// console.log(typeof Boolean(str))
+
+// const falsy1 = ' ';
+// const falsy2 = 0;
+// const falsy3 = undefined;
+// const falsy4 = null;
+// const falsy5 = NaN;
+
+// if (falsy1∥!falsy2) {
+//   console.log('at least one is true')
+// } else {
+//   console.log('only if bouth are false')
+// }
+// console.log('after')
+
+
+// const ziuaSaptaminii = 'o zi ordinara';
+
+// switch (ziuaSaptaminii) {
+//   case 'Luni' === ziuaSaptaminii:
+//     console.log('Astazi este luni!')
+//     break;
+//   case 'Marti' === ziuaSaptaminii:
+//     console.log('Astazi este marti!')
+//     break;
+//   default:
+//     console.log('Nu este zi a saptaminii!')
+// }
+
+
+// const condition = false
+// const condition2 = false
+// const condition3 = false
+
+// const variabila = condition ? 'true' : condition2 ? 'true2' : condition3 ? 'true3' : 'false3';
+// console.log(variabila)
+
+
+
+// "less them an hour ago"
+// "1h 4min"
+// "1d 3h"
+// "3w 1d"
+
+// const lastUpdate = 25000;
+// console.log(${ Match.floor(lastUpdate / 10080) } $(Match.floor(lastUpdate % 10080 / 1440))d)
+// switch (lastUpdate) {
+//   case lastUpdate % 10080 >= 1:
+//     console.log(${ lastUpdate/ 10080} $(lastUpdate % 10080 / 1440)d)
+//   default:
+// console.log("less them an hour ago")
+// }
+
+
+
+// "less them an hour ago"
+// "1h 4min"
+// "1d 3h"
+// "3w 1d"
+// const week = 10080;
+// const day = 1440;
+// const hour = 60;
+// const lastUpdate = 6850;
+// if (lastUpdate / week >= 1) {
+//   console.log(`${Math.floor(lastUpdate / week)}w ${Math.floor(lastUpdate / week / day)}d`)
+// } else if (lastUpdate / day >= 1) {
+//   console.log(`${Math.floor(lastUpdate / week)}d ${Math.floor(lastUpdate / day / hour)}h`)
+// } else if (lastUpdate / hour >= 1) {
+//   console.log(`${Math.floor(lastUpdate / hour)}h ${Math.floor(lastUpdate / hour)}m`)
+// } else {
+//   console.log("less then an hour ago")
+
+
+
+// var variabila = 3
+// // variabila++
+// variabila -= 4
+// console.log(variabila)
+
+
+
+// let a = 5
+// let b = a
+// a = 6
+
+// console.log(a, 'a')
+// console.log(b, 'b')
+
+
+// let a = {
+//   a: 5
+// }
+// let b = a
+// a.a = 6
+
+// console.log(a, 'a')
+// console.log(b, 'b')
+
+
+
+// const arr = [1, 2, 3, 4, 5]
+// const arr2 = arr.slice(3)
+
+// console.log(arr, 'arr')
+// console.log(arr2, 'arr2')
+
+
+
+const variabila = new String('str')
+
+console.log(variabila)
